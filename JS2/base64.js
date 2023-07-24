895b9e22ee225ae6fc876a70e3bf27a095fce7c5
imelon2 <imelon279@naver.com>
2023-07-24 16:36:12 +0900

    base65 ethersV6

--- JS2/base64.js
+++ JS2/base64.js
@@ -1,24 +1,52 @@
 const {ethers,toUtf8Bytes,toUtf8String} =require("ethers");
 
-let NFTMetaData = {
-    "description": "This is Dkargo SBT token",
-    "external_url" : "https://dkargo.io/",
-    "image" : "https://stg-lodis.s3.ap-northeast-2.amazonaws.com/lodiscan/sbt/first-delivery.png",
-    "name" : "Dkargo Carrier SBT Token"
+const LODIS_SBT_IMAGE_URL = {
+    DEV : {
+        Carrier : "https://dev-lodis.s3.ap-northeast-2.amazonaws.com/lodiscan/sbt/first-delivery.png",
+        Shipper : "https://dev-lodis.s3.ap-northeast-2.amazonaws.com/lodiscan/sbt/speed-delivery.png"
+    },
+    STAG : {
+        Carrier : "https://stg-lodis.s3.ap-northeast-2.amazonaws.com/lodiscan/sbt/first-delivery.png",
+        Shipper : "https://stg-lodis.s3.ap-northeast-2.amazonaws.com/lodiscan/sbt/speed-delivery.png"
+    }
 }
 
-let _NFTMetaData = JSON.stringify(NFTMetaData);
+/**
+ * 
+ * @param {DEV,STAG} envir 
+ * @param {Carrier, Shipper} subject 
+ */
+const encodeSBTTokenURL = (envir,subject) => {
+    const NFTMetaData = {
+        "description": "This is Dkargo SBT token",
+        "external_url" : "https://dkargo.io/",
+        "image" : LODIS_SBT_IMAGE_URL[envir][subject],
+        "name" : `Dkargo ${subject} SBT`
+    }
 
-// let NFTMetaData_Base64 = "data:application/json;base64," + Buffer.from(_NFTMetaData).toString("base64");
-// NFTMetaData_Base64 = Buffer.from(a, "base64").toString("utf-8");
+    let _NFTMetaData = JSON.stringify(NFTMetaData);
+    _NFTMetaData = "data:application/json;base64," + ethers.encodeBase64(toUtf8Bytes(_NFTMetaData))
+    return _NFTMetaData;
+}
+
+const decodeSBTTokenURL = (url) => {
+    url = url.replace("data:application/json;base64,","");
+    let NFTMetaData = ethers.decodeBase64(url)
+    NFTMetaData = JSON.parse(toUtf8String(NFTMetaData));
+    return {
+        description : NFTMetaData.description,
+        external_url : NFTMetaData.external_url,
+        image : NFTMetaData.image,
+        name : NFTMetaData.name
+    };
+}
 
 
-let NFTMetaData_Base64_ethers_encode = "data:application/json;base64," + ethers.encodeBase64(toUtf8Bytes(_NFTMetaData))
-console.log(`SBT token URL encode : ${NFTMetaData_Base64_ethers_encode}`);
-NFTMetaData_Base64_ethers_encode = NFTMetaData_Base64_ethers_encode.replace("data:application/json;base64,","");
-let NFTMetaData_Base64_ethers_decode = ethers.decodeBase64(NFTMetaData_Base64_ethers_encode)
+const URL_ENCODE = encodeSBTTokenURL("DEV","Shipper");
+console.log(`Encode URL : ${URL_ENCODE}`);
 
-let parse = JSON.parse(toUtf8String(NFTMetaData_Base64_ethers_decode))
-console.log(parse.image);
-
-
+const URL_DECODE = decodeSBTTokenURL(URL_ENCODE);
+console.log(`Encode URL : ${URL_DECODE.description}`);
+console.log(`Encode URL : ${URL_DECODE.external_url}`);
+console.log(`Encode URL : ${URL_DECODE.image}`);
+console.log(`Encode URL : ${URL_DECODE.name}`);
